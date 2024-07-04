2ef40f6cdde7485e9632dd666913e4e09c490c9b
MiguelwLD <114840612+Miguel7293@users.noreply.github.com>
2024-07-04 11:48:17 -0500

    arreglado
    ya se puede reanudar el proyecto

--- proyecto_plataformas-esta-confe/app/src/main/java/llusca/migugoty/projecthub/iniciooptions.kt
+++ proyecto_plataformas-esta-confe/app/src/main/java/llusca/migugoty/projecthub/iniciooptions.kt
@@ -13,7 +13,7 @@ class iniciooptions : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_iniciooptions)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
